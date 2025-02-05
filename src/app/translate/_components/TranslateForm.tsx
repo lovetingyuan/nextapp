0cd67b5be8dfa298de221e37ede9dd72ba191d93
tingyuan <1932294867@qq.com>
2025-02-05 11:16:57 +0800

    feat: add title and kbd

---
+++ src/app/translate/_components/TranslateForm.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { useCompletion } from 'ai/react'
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { Check, Copy, LoaderPinwheel } from 'lucide-react'
+
+export default function TranslateForm() {
+  const [isCopied, setIsCopied] = React.useState(false)
+  const { completion, input, handleInputChange, error, handleSubmit, isLoading } = useCompletion({
+    api: '/api/completion',
+  })
+
+  const formRef = React.useRef<HTMLFormElement>(null)
+
+  const handleCopy = async () => {
+    if (isCopied) {
+      return
+    }
+    await navigator.clipboard.writeText(completion || '')
+    setIsCopied(true)
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 2000)
+  }
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
+      e.preventDefault()
+      formRef.current?.requestSubmit()
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <Textarea
+          placeholder="Enter text to translate"
+          className="w-full min-h-60 mt-6 field-sizing-content"
+          value={input}
+          autoFocus
+          name="prompt"
+          onKeyDown={handleKeyDown}
+          disabled={isLoading}
+          onChange={handleInputChange}
+        />
+        <Button type="submit" disabled={isLoading} className="mt-8 w-full" size="lg">
+          {isLoading ? (
+            <span>Translating...</span>
+          ) : (
+            <span>
+              Translate (<kbd>Ctrl</kbd>+<kbd>Enter</kbd>)
+            </span>
+          )}
+        </Button>
+      </form>
+
+      {isLoading || completion || error ? (
+        <div className="group border border-gray-200 rounded-md p-4 mt-8 flow-root relative">
+          {isLoading && !completion && (
+            <LoaderPinwheel className="animate-spin" color="cornflowerblue" />
+          )}
+          <pre className="whitespace-pre-wrap">{completion}</pre>
+          {error ? <p className="text-red-600 italic">Error: {error.message}</p> : null}
+          <Button
+            variant="ghost"
+            size="icon"
+            className="invisible group-hover:visible absolute top-0 right-0 p-0"
+            onClick={handleCopy}
+            title="copy"
+          >
+            {isCopied ? <Check /> : <Copy />}
+          </Button>
+        </div>
+      ) : null}
+    </>
+  )
+}
--- src/app/translate/page.tsx
+++ src/app/translate/page.tsx
@@ -1,74 +1,21 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { Textarea } from '@/components/ui/textarea'
-import { useState, useRef } from 'react'
-import { Check, Copy, Languages, LoaderPinwheel } from 'lucide-react'
-import { useCompletion } from 'ai/react'
+import { Languages } from 'lucide-react'
+
+import type { Metadata } from 'next'
+import TranslateForm from './_components/TranslateForm'
+
+export const metadata: Metadata = {
+  title: 'Translate', // 设置页面标题
+  description: 'translate english helper', // 可选：设置页面描述
+}
 
 export default function Translate() {
-  const [isCopied, setIsCopied] = useState(false)
-  const { completion, input, handleInputChange, error, handleSubmit, isLoading } = useCompletion({
-    api: '/api/completion',
-  })
-
-  const formRef = useRef<HTMLFormElement>(null)
-
-  const handleCopy = async () => {
-    if (isCopied) {
-      return
-    }
-    await navigator.clipboard.writeText(completion || '')
-    setIsCopied(true)
-    setTimeout(() => {
-      setIsCopied(false)
-    }, 2000)
-  }
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter' && (e.ctrlKey || e.metaKey)) {
-      e.preventDefault()
-      formRef.current?.requestSubmit()
-    }
-  }
   return (
     <div className="px-4 pt-4 pb-12 sm:p-10 max-w-screen-md mx-auto">
       <h2 className="text-2xl font-normal">
         中英小翻译
         <Languages className="w-6 h-6 inline-block ml-2" />
       </h2>
-      <form onSubmit={handleSubmit} ref={formRef}>
-        <Textarea
-          placeholder="Enter text to translate"
-          className="w-full min-h-60 mt-6 field-sizing-content"
-          value={input}
-          autoFocus
-          name="prompt"
-          onKeyDown={handleKeyDown}
-          disabled={isLoading}
-          onChange={handleInputChange}
-        />
-        <Button type="submit" disabled={isLoading} className="mt-8 w-full" size="lg">
-          {isLoading ? 'Translating...' : 'Translate'}
-        </Button>
-      </form>
-
-      {isLoading || completion || error ? (
-        <div className="group border border-gray-200 rounded-md p-4 mt-8 flow-root relative">
-          {isLoading && !completion && (
-            <LoaderPinwheel className="animate-spin" color="cornflowerblue" />
-          )}
-          <pre className="whitespace-pre-wrap">{completion}</pre>
-          {error ? <p className="text-red-600 italic">Error: {error.message}</p> : null}
-          <Button
-            variant="ghost"
-            size="icon"
-            className="invisible group-hover:visible absolute top-0 right-0 p-0"
-            onClick={handleCopy}
-            title="copy"
-          >
-            {isCopied ? <Check /> : <Copy />}
-          </Button>
-        </div>
-      ) : null}
+      <TranslateForm />
     </div>
   )
 }
