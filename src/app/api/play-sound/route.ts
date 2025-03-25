fa36fbb6732f3e5170e1974747805050a977c5b4
tingyuan <1932294867@qq.com>
2025-03-26 01:07:45 +0800

    -

--- src/app/api/play-sound/route.ts
+++ src/app/api/play-sound/route.ts
@@ -2,33 +2,33 @@ import { NextRequest } from 'next/server'
 
 export const runtime = 'edge'
 
-export async function _POST(req: NextRequest) {
-  const { text } = (await req.json()) as { text: string }
+//   async function _POST(req: NextRequest) {
+//   const { text } = (await req.json()) as { text: string }
 
-  const url = 'https://api.deepgram.com/v1/speak?model=aura-asteria-en'
+//   const url = 'https://api.deepgram.com/v1/speak?model=aura-asteria-en'
 
-  const body = JSON.stringify({
-    text,
-  })
+//   const body = JSON.stringify({
+//     text,
+//   })
 
-  const headers = {
-    Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`,
-    'Content-Type': 'application/json',
-  }
+//   const headers = {
+//     Authorization: `Token ${process.env.DEEPGRAM_API_KEY}`,
+//     'Content-Type': 'application/json',
+//   }
 
-  const options = {
-    method: 'POST',
-    headers: headers,
-    body: body,
-  }
+//   const options = {
+//     method: 'POST',
+//     headers: headers,
+//     body: body,
+//   }
 
-  return fetch(url, options).then(response => {
-    if (!response.ok) {
-      throw new Error('Failed to make request to deepgram:' + response.statusText)
-    }
-    return response
-  })
-}
+//   return fetch(url, options).then(response => {
+//     if (!response.ok) {
+//       throw new Error('Failed to make request to deepgram:' + response.statusText)
+//     }
+//     return response
+//   })
+// }
 
 export async function POST(req: NextRequest) {
   const { text } = (await req.json()) as { text: string }
